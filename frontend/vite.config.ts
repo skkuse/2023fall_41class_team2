607e44e7fee6fa61da18df6f1f8521258e0f2a34
권동민 <gwondongmin@gwondongmin-ui-MacBookPro.local>
2023-09-18 18:38:55 +0900

    :wrench: Add path alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import removeConsole from 'vite-plugin-remove-console';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), removeConsole()],
   envDir: './config',
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 });
