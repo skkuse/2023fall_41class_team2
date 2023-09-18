fa0ab28e6c76e3b2bde8d6164621e3634c228a64
권동민 <gwondongmin@gwondongmin-ui-MacBookPro.local>
2023-09-18 20:02:32 +0900

    :sparkles: Add routing, scss

---
+++ frontend/src/components/common/Header.tsx
@@ -0,0 +1,9 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: MIT
+import './Header.scss';
+
+const Header = () => {
+  return <div className="header"></div>;
+};
+
+export default Header;
