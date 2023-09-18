3563748eb53dd925a1faf55e90b6636de03ac4ea
권동민 <gwondongmin@gwondongmin-ui-MacBookPro.local>
2023-09-18 20:02:32 +0900

    :sparkles: Add routing, scss

---
+++ frontend/src/components/common/Footer.tsx
@@ -0,0 +1,10 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: MIT
+
+import './Footer.scss';
+
+const Footer = () => {
+  return <div className="footer"></div>;
+};
+
+export default Footer;
