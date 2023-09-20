29ae8fd9836f009c45e1cd7e263669f5b1a90235
Yanghyeondong <hdyang0686@naver.com>
2023-09-20 05:44:26 +0000

    ✨ connect fast api and java module

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,34 @@
-from typing import Union
-
-from fastapi import FastAPI
+from typing import Union, Annotated
+from fastapi import FastAPI, HTTPException, Body
+from pydantic import BaseModel
+from module import java_exec
 
 app = FastAPI()
 
 
+class Code(BaseModel):
+    req_code: str
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"info":"Welcome SWE2023 Team2!"}
+
+@app.post("/run_java")
+def run_java(
+    code: Annotated [
+        Code,
+        Body(
+            examples=[
+                {
+                    "req_code":  
+                    """public class HelloSKKU { public static void main(String[ ] args) { System.out.println("Hello SKKU!"); }}"""
+                }
+            ],
+        ),
+    ],
+):
+    dicted_code = dict(code)
+    java_code = dicted_code['req_code']
+    runtime_info = java_exec.runtime_results(java_code)
+
+    return(runtime_info)
