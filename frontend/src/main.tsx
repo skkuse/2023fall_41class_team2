8c313f84b0169b80eab8e7c61e84c0c493b79b97
clapsh <psh8295015@gmail.com>
2023-11-16 03:01:59 +0900

    Add main page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
+import MainPage from './pages/MainPage.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 
@@ -17,6 +18,17 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/main',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage/>,
       },
     ],
     errorElement: <ErrorPage />,
