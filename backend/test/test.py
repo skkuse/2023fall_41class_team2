06e896f5781018a026a8008911068dea888a8244
Gyunseo Lee <rbstj0311@naver.com>
2023-12-07 07:44:59 +0900

    test(be): locust 부하 테스트 스크립트 작성

---
+++ backend/test/test.py
@@ -0,0 +1,17 @@
+from locust import HttpUser, task, TaskSet
+
+
+class MainBehavior(TaskSet):
+    @task
+    def main_task(self):
+        self.client.post(
+            "/api/carbon_emission_calculate",
+            json={
+                "req_code": 'public class HelloSKKU { public static void main(String[ ] args) { for(int i = 0; i < 10; i++){ System.out.println("Hello SKKU!"); System.err.println("Bye SKKU!");}}}'
+            },
+        )
+
+
+class LocustUser(HttpUser):
+    host = "http://0.0.0.0:8000"
+    tasks = [MainBehavior]
